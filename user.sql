9822a75196f92189f54fbfd74d5359f6a5b7d5e7
Jose Manuel <josemhp709@gmail.com>
2017-09-04 17:13:41 -0600

    script user update

--- user.sql
+++ user.sql
@@ -1,3 +1,4 @@
+--Crear usuario y dar permisos.
 create user bases identified by bases;
 grant connect, resource, dba to bases;
 grant unlimited tablespace to bases;
@@ -13,7 +14,8 @@ GRANT create any table TO bases;
 --User works as a schema.
 --Log in
 connect bases/bases; --
-
+ALTER USER bases DEFAULT TABLESPACE Bschema; --ASignar un tablespace al usuario!!
+CREATE TABLESPACE Bschema DATAFILE 'tbs_f2.dbf' SIZE 40M     ONLINE;--Crear un tablespace
 /* grant permission to other users on a schema
 GRANT
   SELECT,
@@ -26,6 +28,24 @@ TO
   books_admin;
 */ 
 --Create tables on user bases
-create table t1(a int, b int) on bases; --No funciona :v
+create table bases.t1(a int, b int);
 create index pkt1(a) on t1 on index;
 unique alter table add contraint pkt1 primary key;
+
+
+--Consultas
+select df.tablespace_name "Tablespace",
+       totalusedspace "Used MB",
+       (df.totalspace - tu.totalusedspace) "Free MB",
+       df.totalspace "Total MB",
+       round(100 * ( (df.totalspace - tu.totalusedspace)/ df.totalspace)) "Pct. Free"
+  from (select tablespace_name,
+               round(sum(bytes) / 1048576) TotalSpace
+          from dba_data_files 
+         group by tablespace_name) df,
+       (select round(sum(bytes)/(1024*1024)) totalusedspace,
+               tablespace_name
+          from dba_segments 
+         group by tablespace_name) tu
+ where df.tablespace_name = tu.tablespace_name 
+   and df.totalspace <> 0;
